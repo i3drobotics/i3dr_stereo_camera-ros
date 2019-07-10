93cdec177ad529ba5d52e85016a41e29c3937992
bcrutchleyi3dr <bcrutchley@i3drobotics.com>
2019-07-10 17:40:17 +0100

    added option to build without i3dr matchher

--- cmake/FindPhobos.cmake
+++ cmake/FindPhobos.cmake
@@ -1,19 +1,25 @@
-#set(PHOBOS_ROOT $ENV{PHOBOS_ROOT})
-#if (NOT DEFINED ENV{PHOBOS_ROOT})
-#    set(PHOBOS_ROOT "/usr/local/Phobos")
-#endif()
+cmake_minimum_required(VERSION 2.6)
+
 set (Phobos_ROOT_DIR "/usr/local/Phobos")
 
-#find_path(PHOBOS_INCLUDE_DIR
-#    ${Phobos_ROOT_DIR}/include
-#    DOC "The Phobos include directory"
-#)
+# OpenCV 3.4.6 Required for I3DR stereo algorithms
+find_package(OpenCV 3.4.6 REQUIRED)
+
+set (PHOBOS_INCLUDE_DIR ${Phobos_ROOT_DIR}/include)
+
+if (NOT PHOBOS_INCLUDE_DIR)
+    message(WARNING "include directory not found")
+endif()
 
 find_library(PHOBOS_LIBRARY 
     NAMES PhobosIntegration
     PATHS ${Phobos_ROOT_DIR}/lib
-    DOC "The Phobos library"
+    DOC "The Phobos library "
 )
+
+if (NOT PHOBOS_LIBRARY)
+    message(WARNING "library directory not found")
+endif()
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LOGGING_FOUND to TRUE
@@ -22,11 +28,11 @@ find_package_handle_standard_args(PHOBOS DEFAULT_MSG PHOBOS_INCLUDE_DIR PHOBOS_L
 
 if (PHOBOS_FOUND)
     set(Phobos_LIBRARIES ${PHOBOS_LIBRARY} )
-    set(Phobos_INCLUDE_DIRS ${Phobos_ROOT_DIR}/include )
+    set(Phobos_INCLUDE_DIRS ${PHOBOS_INCLUDE_DIR} )
     set(Phobos_DEFINITIONS )
 else(PHOBOS_FOUND)
-    message(FATAL_ERROR "Phobos not found")
+    message(WARNING "Phobos not found")
 endif()
 
 # Tell cmake GUIs to ignore the "local" variables.
-mark_as_advanced(Phobos_ROOT_DIR PHOBOS_INCLUDE_DIR PHOBOS_LIBRARY)
+mark_as_advanced(Phobos_ROOT_DIR PHOBOS_LIBRARY PHOBOS_LIBRARY)
