b86231c8448e743f4707090793af518c5500e782
Ben <benknight135@gmail.com>
2022-07-01 17:34:57 +0100

    Update init_stereo_matchers.cpp

--- src/init_stereo_matchers.cpp
+++ src/init_stereo_matchers.cpp
@@ -10,8 +10,6 @@
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/core/core.hpp>
 
-#include <opencv2/ximgproc.hpp>
-
 #include <image_transport/image_transport.h>
 
 #include <boost/filesystem.hpp>
