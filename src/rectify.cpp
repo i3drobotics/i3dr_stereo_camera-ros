eb1f42429a2519e12b895796d6ee42b3ef350bcf
Ben <benknight135@gmail.com>
2022-07-04 16:12:18 +0100

    add opencv header for moved 'initUndistortRectifyMap' function

--- src/rectify.cpp
+++ src/rectify.cpp
@@ -13,6 +13,7 @@
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
 #include <opencv2/imgcodecs.hpp>
 
 #include <image_transport/image_transport.h>
